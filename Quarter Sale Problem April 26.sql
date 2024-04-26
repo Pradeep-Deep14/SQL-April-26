298cf82d8a7ea778632bfdfc70a22c652f31c76c
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-04-26 20:46:46 +0530

    Add files via upload

---
+++ Quarter Sale Problem April 26.sql
@@ -0,0 +1,56 @@
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    quantity INT
+);
+
+INSERT INTO orders 
+(order_id, order_date, quantity) 
+VALUES
+(1, '2023-01-02', 5),
+(2, '2023-02-05', 3),
+(3, '2023-02-07', 2),
+(4, '2023-03-10', 6),
+(5, '2023-02-15', 4),
+(6, '2023-04-21', 8),
+(7, '2023-05-28', 7),
+(8, '2023-05-05', 3),
+(9, '2023-08-10', 5),
+(10, '2023-05-02', 6),
+(11, '2023-02-07', 4),
+(12, '2023-04-15', 9),
+(13, '2023-03-22', 7),
+(14, '2023-04-30', 8),
+(15, '2023-04-05', 6),
+(16, '2023-02-02', 6),
+(17, '2023-01-07', 4),
+(18, '2023-05-15', 9),
+(19, '2023-05-22', 7),
+(20, '2023-06-30', 8),
+(21, '2023-07-05', 6);
+
+SELECT * FROM ORDERS
+
+--For each week, find the total number of orders
+
+SELECT SUM(QUANTITY) AS TOTAL_QUANTITY,
+       EXTRACT(WEEK FROM ORDER_DATE) AS WEEK
+FROM ORDERS
+WHERE 
+EXTRACT(YEAR FROM ORDER_DATE)=2023
+AND
+EXTRACT(QUARTER FROM ORDER_DATE)=1
+GROUP BY WEEK
+
+
+--FIND EACH QUARTER AND TOTAL QUANTIY SALE
+
+SELECT EXTRACT(QUARTER FROM ORDER_DATE) AS QUARTER,
+       SUM(QUANTITY) AS TOTAL_QUANTITY
+FROM ORDERS
+GROUP BY QUARTER
+
+
+
